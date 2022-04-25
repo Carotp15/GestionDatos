368d55d3246edd6f025ba63ab8f194bb4fa89e43
JohnnyGP <98815823+JohnnyGP@users.noreply.github.com>
2022-04-24 21:44:33 -0500

    Add files via upload

---
+++ Taller3/conexion_sqlserver.py
@@ -0,0 +1,47 @@
+import pyodbc
+
+try:
+    conexion = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-1KCUJN2;DATABASE=Chinook;UID=Johnny;PWD=12345')
+   
+    print("Conexión exitosa.")
+
+    # Insertar datos 
+    with conexion.cursor() as cursor:
+        consulta = "INSERT INTO Album VALUES (?, ?, ?);"
+        cursor.execute(consulta, (1945, "God's fall", 275))
+        cursor.execute(consulta, (1994, "Requiem", 275))
+        cursor.execute(consulta, (2017, "Sunny Days", 275))
+        cursor.execute(consulta, (2014, "Superpowers", 275))    
+
+    # Consulta de Empleado 
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT EmployeeId, FirstName, City FROM Employee;")
+        Employee = cursor.fetchall()
+
+        for empleado in Employee:
+            print(empleado)
+    # Actualizar datos
+    with conexion.cursor() as cursor:
+
+        consulta = "UPDATE Album SET Title = ? WHERE AlbumId = ?;"
+        nuevo_album = "Avant"
+        id_nuevo = 347
+        cursor.execute(consulta, (nuevo_album, id_nuevo))
+    conexion.commit()
+
+    # Eliminar datos
+    with conexion.cursor() as cursor:
+
+        consulta = "DELETE FROM Album WHERE AlbumId = ?;"
+        AlbumId = 1945
+        cursor.execute(consulta, (AlbumId))
+
+    conexion.commit()
+
+
+
+except Exception as ex:
+    print("Error durante la conexión: {}".format(ex))
+finally:
+    conexion.close()  # Se cerró la conexión a la BD.
+    print("La conexión ha finalizado.")
